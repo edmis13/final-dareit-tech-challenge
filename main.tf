a130e14db37c42a43369c0403e5792de68025436
edmis13 <124311607+edmis13@users.noreply.github.com>
2023-04-12 19:53:00 +0200

    feat: add terraform files

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "google_compute_instance" "dareit-vm-ci" {
+  name         = "dareit-vm-tf-ci"
+  machine_type = "e2-small"
+  zone         = "us-central1-a"
+  tags         = ["tech"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        managed_by_terraform = "true"
+      }
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
+
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = "western-cascade-378410"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
